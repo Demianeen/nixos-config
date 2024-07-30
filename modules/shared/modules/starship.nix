daed2e6f546b9df7120ee4e4232f8bdc17329ee1
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-07-30 13:40:49 +0100

    feat(home-manager): add custom starship addons

--- modules/shared/modules/starship.nix
+++ modules/shared/modules/starship.nix
@@ -13,6 +13,33 @@
         truncation_length = 4;
         style = "bold lavender";
       };
+
+      custom = {
+        git_server_icon = {
+          description =
+            "Show a GitLab or GitHub icon depending on current git remote";
+          when = "git rev-parse --is-inside-work-tree 2> /dev/null";
+          command = ''
+            GIT_REMOTE=$(git ls-remote --get-url 2> /dev/null); if [[ "$GIT_REMOTE" =~ "github" ]]; then printf ""; elif [[ "$GIT_REMOTE" =~ "gitlab" ]]; then echo ""; else echo "󰊢"; fi'';
+          shell = [ "bash" "--noprofile" "--norc" ];
+          style = "bold #f74e27"; # git brand color
+          format = "[$output]($style) ";
+        };
+        nix_shell = {
+          description = "Show an indicator when inside a Nix ephemeral shell";
+          when = ''[ "$IN_NIX_SHELL" != "" ]'';
+          shell = [ "bash" "--noprofile" "--norc" ];
+          style = "bold #6ec2e8";
+          format = "[ ]($style)";
+        };
+        direnv = {
+          description = "Show '.envrc' when using a direnv environment";
+          when = ''[ "$DIRENV_DIR" != "" ] && [ "$IN_NIX_SHELL" != "" ]'';
+          shell = [ "bash" "--noprofile" "--norc" ];
+          style = "italic #f9e2af";
+          format = "[via](italic #586068) [.envrc]($style)";
+        };
+      };
     };
   };
 }
