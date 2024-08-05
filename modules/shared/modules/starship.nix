1c313607c359ee0d564a8391d989d8c4e61fc68b
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-08-05 16:27:31 +0100

    feat(home-manager): make a starship one line

--- modules/shared/modules/starship.nix
+++ modules/shared/modules/starship.nix
@@ -1,13 +1,14 @@
-{
+{ lib, ... }: {
   programs.starship = {
     enable = true;
     settings = {
-      package.disabled = true;
-      nodejs.disabled = true;
-      lua.disabled = true;
-      gcloud.disabled = true;
-      git_status.disabled = true;
-      cmd_duration.disabled = true;
+      add_newline = false;
+      format = lib.concatStrings [
+        "$directory"
+        "\${custom.git_server_icon}"
+        "\${custom.nix_shell}"
+        "$character"
+      ];
 
       directory = {
         truncation_length = 4;
@@ -23,7 +24,7 @@
             GIT_REMOTE=$(git ls-remote --get-url 2> /dev/null); if [[ "$GIT_REMOTE" =~ "github" ]]; then printf ""; elif [[ "$GIT_REMOTE" =~ "gitlab" ]]; then echo ""; else echo "󰊢"; fi'';
           shell = [ "bash" "--noprofile" "--norc" ];
           style = "bold #f74e27"; # git brand color
-          format = "[$output]($style) ";
+          format = "[$output]($style)";
         };
         nix_shell = {
           description = "Show an indicator when inside a Nix ephemeral shell";
@@ -31,13 +32,6 @@
           shell = [ "bash" "--noprofile" "--norc" ];
           style = "bold #6ec2e8";
           format = "[ ]($style)";
-        };
-        direnv = {
-          description = "Show '.envrc' when using a direnv environment";
-          when = ''[ "$DIRENV_DIR" != "" ] && [ "$IN_NIX_SHELL" != "" ]'';
-          shell = [ "bash" "--noprofile" "--norc" ];
-          style = "italic #f9e2af";
-          format = "[via](italic #586068) [.envrc]($style)";
         };
       };
     };
