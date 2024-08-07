3194eb19e8b59b1f7cbffc43498ab7b8c4fc8b3a
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-08-07 10:23:39 +0100

    feat(fzf): setup fzf

--- modules/shared/modules/fzf.nix
+++ modules/shared/modules/fzf.nix
@@ -1,1 +1,25 @@
-{ programs.fzf = { enable = true; }; }
+{ pkgs, lib, ... }: {
+  programs.fzf = {
+    enable = true;
+    defaultCommand = "${pkgs.ripgrep}/bin/rg --files";
+
+    # Command for the CTRL-T file widget
+    fileWidgetCommand = "${pkgs.ripgrep}/bin/rg --files";
+    fileWidgetOptions = [
+      # Preview files with bat
+      "--preview '${pkgs.bat}/bin/bat --color=always {}'"
+      "--layout default"
+    ];
+
+    # Command for the ALT-C directory widget
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+    changeDirWidgetOptions =
+      [ "--preview '${pkgs.eza}/bin/exa --tree --level=1 --color=always {}'" ];
+  };
+  programs.zsh = {
+    initExtra = ''
+      # macos keymaps
+      bindkey "ç" fzf-cd-widget # option+c
+    '';
+  };
+}
