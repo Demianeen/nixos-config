2bf7bc98ea284d040dc8ccbe5785db56ccd0f0ee
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-10-30 10:46:09 +0000

    chore(sketchybar): add stdout and stderr file paths for logging

--- modules/darwin/modules/sketchybar/sketchybar.nix
+++ modules/darwin/modules/sketchybar/sketchybar.nix
@@ -1,13 +1,19 @@
 { pkgs, ... }:
 
-let sbar-lua = pkgs.callPackage ./sbar-lua.nix { };
-in {
+let
+  sbar-lua = pkgs.callPackage ./sbar-lua.nix { };
+in
+{
   config = {
     services = {
       sketchybar = {
         enable = true;
         extraPackages = [ pkgs.lua5_4 ];
       };
+    };
+    launchd.user.agents.sketchybar.serviceConfig = {
+      StandardOutPath = "/tmp/sketchybar-logs/sketchybar.stdout.log";
+      StandardErrorPath = "/tmp/sketchybar-logs/sketchybar.stderr.log";
     };
   };
 }
