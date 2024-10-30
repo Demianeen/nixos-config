b618b8ea102082204f7c89698d9b822bbf8676d0
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-10-30 10:13:54 +0000

    chore(packages): remove yabai, skhd, sketchybar and jankyborders from PATH as they are isolated within Nix service

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,21 +1,18 @@
 { pkgs }:
 
 with pkgs;
-let shared-packages = import ../shared/packages.nix { inherit pkgs; };
-in shared-packages ++ [
+let
+  shared-packages = import ../shared/packages.nix { inherit pkgs; };
+in
+shared-packages
+++ [
   darwin.trash
 
   # system
   dockutil
 
-  # navigation
-  yabai
-  skhd
-
   # ui
-  sketchybar
   sketchybar-app-font
-  jankyborders
 
   # password manager
   _1password
