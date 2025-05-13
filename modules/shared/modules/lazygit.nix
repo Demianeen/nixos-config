fce4a3dfc7f1d7ad78a0a2f569927aefc2fc0fad
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2025-05-13 17:47:06 +0100

    feat(lazygit): auto forward all branches

--- modules/shared/modules/lazygit.nix
+++ modules/shared/modules/lazygit.nix
@@ -22,6 +22,7 @@
         showCommandLog = false;
       };
       git = {
+        autoForwardBranches = "allBranches";
         overrideGpg = true; # overrides lazygit gpg restrictions
         branchLogCmd = "git lg {{branchName}}";
         paging = {
